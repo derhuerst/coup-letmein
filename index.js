1bd3c4c9be3bd3bf1a74bb9e60d01aaa5387a263
Jannis R <mail@jannisr.de>
2016-07-16 17:27:52 +0200

    expect HTTP 2xx status code

--- index.js
+++ index.js
@@ -14,4 +14,12 @@ ${err.code} error, connecting to port ${err.port} at ${err.address}.\n`)
 	process.exit(1)
 })
 
+req.on('response', (res) => {
+	if (res.statusCode < 200 || res.statusCode >= 300) {
+		process.stdout.write(`\
+${res.statusCode} ${res.statusMessage}\n`)
+		process.exit(1)
+	}
+})
+
 req.end() // send request without a body
